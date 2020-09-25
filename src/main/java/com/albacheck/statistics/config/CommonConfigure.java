a80f8a6014e05a25cb10215a6a5db1c88822b8f8
Choi Byunghyeon <choi8608@gmail.com>
2020-09-25 17:26:28 +0900

    refactor: StatisticsConfigure 를 CommonConfigure 로 변경

--- src/main/java/com/albacheck/statistics/config/CommonConfigure.java
+++ src/main/java/com/albacheck/statistics/config/CommonConfigure.java
@@ -6,12 +6,10 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Configuration
-public class StatisticsConfigure {
+public class CommonConfigure {
 
   public static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
   public static final JsonFactory JSON_FACTORY = new JacksonFactory();
